fe65f862002bd2975ff58437308e6844b5651253
wick3dr0se <wick3dr0se@protonmail.com>
2024-06-06 02:14:58 -0400

    drop IVec2 (simplified world_to_map/map_to_world). moved draw_isometric_tiles out of Map

--- src/map.rs
+++ src/map.rs
@@ -2,7 +2,7 @@ use super::*;
 
 use macroquad_tiled as tiled;
 
-const TILE_SIZE: IVec2 = ivec2(32, 32);
+const TILE_SIZE: Vec2 = vec2(64.0, 64.0);
 
 pub struct Map {
     tilemap: tiled::Map
@@ -22,44 +22,44 @@ impl Map {
         Self { tilemap }
     }
 
-    pub fn draw(&self) {        
-        self.draw_isometric_tiles("Base");
-        self.draw_isometric_tiles("Props");
+    pub fn draw(&self) {
+        draw_isometric_tiles(&self.tilemap, "Base");
+        draw_isometric_tiles(&self.tilemap, "Props");
     }
+}
 
-    fn draw_isometric_tiles(&self, layer: &str) {
-        let map_layer = &self.tilemap.layers[layer];
-    
-        for y in 0..map_layer.height {
-            for x in 0..map_layer.width {
-                if let Some(tile) = self.tilemap.get_tile(layer, x, y) {
-                    let world_pos = map_to_world(ivec2(x as i32, y as i32));
+fn draw_isometric_tiles(tilemap: &tiled::Map, layer: &str) {
+    let map_layer = &tilemap.layers[layer];
 
-                    tiled::Map::spr(
-                        &self.tilemap,
-                        "tileset",
-                        tile.id,
-                        Rect::new(world_pos.x, world_pos.y, 32.0, 32.0)
-                    );
-                }
+    for y in 0..map_layer.height {
+        for x in 0..map_layer.width {
+            if let Some(tile) = tilemap.get_tile(layer, x, y) {
+                let world_pos = map_to_world(vec2(x as f32, y as f32));
+
+                tiled::Map::spr(
+                    &tilemap,
+                    "tileset",
+                    tile.id,
+                    Rect::new(world_pos.x, world_pos.y, 64.0, 64.0)
+                );
             }
         }
     }
 }
 
-fn world_to_map(world_pos: Vec2) -> IVec2 {
-    let ihat = vec2(0.5, 0.25) * TILE_SIZE.as_vec2();
-    let jhat = vec2(-0.5, 0.25) * TILE_SIZE.as_vec2();
+pub fn world_to_map(world_pos: Vec2) -> Vec2 {
+    let ihat = vec2(0.5, 0.25) * TILE_SIZE;
+    let jhat = vec2(-0.5, 0.25) * TILE_SIZE;
     let inverse = mat2(ihat, jhat).inverse();
 
-    inverse.mul_vec2(world_pos).as_ivec2()
+    inverse.mul_vec2(world_pos)
 }
 
-fn map_to_world(map_pos: IVec2) -> Vec2 {
-    let ihat = vec2(0.5, 0.25) * TILE_SIZE.as_vec2();
-    let jhat = vec2(-0.5, 0.25) * TILE_SIZE.as_vec2();
+pub fn map_to_world(map_pos: Vec2) -> Vec2 {
+    let ihat = vec2(0.5, 0.25) * TILE_SIZE;
+    let jhat = vec2(-0.5, 0.25) * TILE_SIZE;
     let transform = mat2(ihat, jhat);
-    let offset = ivec2(-TILE_SIZE.x / 2, 0);
+    let offset = vec2(-TILE_SIZE.x / 2.0, 0.0);
 
-    transform.mul_vec2(map_pos.as_vec2()) + offset.as_vec2()
+    transform.mul_vec2(map_pos) + offset
 }
