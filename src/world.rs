db01372133b0449968346d0cdc845e26f57cb260
wick3dr0se <wick3dr0se@protonmail.com>
2024-06-12 12:19:07 -0400

    draw fps, iterate over non-walkable tiles to check for collision..

--- src/world.rs
+++ src/world.rs
@@ -1,6 +1,9 @@
 use super::*;
+use animation::Animation;
 use map::*;
 use entity::*;
+
+const WOLF_TEX_PATH: &str = "assets/critters/wolf/wolf-all.png";
 
 pub struct World {
     map: Map,
@@ -13,24 +16,24 @@ pub struct World {
 impl World {
     pub async fn new() -> Self {
         let wolf_animations = vec![
-            Entity::animation("idle_up", 11, 8, 5),
-            Entity::animation("idle_left", 10, 8, 5),
-            Entity::animation("idle_down", 8, 8, 5),
-            Entity::animation("idle_right", 9, 8, 5),
-            Entity::animation("walk_up", 15, 8, 15),
-            Entity::animation("walk_left", 14, 8, 15),
-            Entity::animation("walk_down", 12, 8, 15),
-            Entity::animation("walk_right", 13, 8, 15)
+            Animation { name: "idle_up".to_string(), row: 11, frames: 8, fps: 5 },
+            Animation { name: "idle_left".to_string(), row: 10, frames: 8, fps: 5 },
+            Animation { name: "idle_down".to_string(), row: 8, frames: 8, fps: 5 },
+            Animation { name: "idle_right".to_string(), row: 9, frames: 8, fps: 5 },
+            Animation { name: "walk_up".to_string(), row: 15, frames: 8, fps: 15 },
+            Animation { name: "walk_left".to_string(), row: 14, frames: 8, fps: 15 },
+            Animation { name: "walk_down".to_string(), row: 12, frames: 8, fps: 15 },
+            Animation { name: "walk_right".to_string(), row: 13, frames: 8, fps: 15 }
         ];
         let mut enemies = Vec::new();
         
-        for _ in 0..25 {
+        for _ in 0..100 {
             enemies.push(
                 Entity::new(
                     rand::gen_range(-640.0, 640.0),
                     rand::gen_range(320.0, 1280.0),
                     32.0, 32.0,
-                    "assets/critters/wolf/wolf-all.png",
+                    WOLF_TEX_PATH,
                     wolf_animations.clone()
                 ).await
             )
@@ -44,7 +47,7 @@ impl World {
             )),
             player: Entity::new(
                 0.0, 640.0, 32.0, 32.0,
-                "assets/critters/wolf/wolf-all.png",
+                WOLF_TEX_PATH,
                 wolf_animations
             ).await,
             enemies,
@@ -53,35 +56,48 @@ impl World {
     }
 
     pub fn update(&mut self) {
+        draw_text(&format!("FPS: {}", get_fps()), -25.0, 0.0, 30.0, BLUE);
+
         self.camera.target = vec2(
             self.player.rect.x + self.player.rect.w / 2.0,
             self.player.rect.y + self.player.rect.h / 2.0
         );
 
-        //self.map.update();
+        self.map.update();
         self.player.update();
         self.player.keyboard_controller();
 
-        for collider in &self.map.colliders {
-            if self.player.aabb(collider) {
-                println!("{}: Encountered a prop", get_time());
+        for tile in &self.map.tiles {
+            if !tile.walkable {
+                if self.player.aabb(&tile.rect) {
+                    println!("{}: Encountered a prop", get_time());
+
+                    self.player.collide(&tile.rect);
+                    break;
+                }
             }
         }
 
         for enemy in &mut self.enemies {
             enemy.update();
-            
+
             if self.player.aabb(&enemy.rect) {
                 println!("{}: Encountered an enemy", get_time());
-            }
 
-            /* not the most performant
-            for collider in &self.map.colliders {
-                if enemy.aabb(collider) {
-                    println!("{}: Enemy encounted a prop", get_time());
+                self.player.collide(&enemy.rect);
+                break;
+            }
+            
+            for tile in &self.map.tiles {
+                if !tile.walkable {
+                    if enemy.aabb(&tile.rect) {
+                        println!("{}: Enemy encounted a prop", get_time());
+
+                        enemy.collide(&tile.rect);
+                        break;
+                    }
                 }
             }
-            */
         }
 
         if get_time() - self.time > 1.0 {
@@ -94,10 +110,10 @@ impl World {
     }
 
     pub fn draw(&mut self) {
-        set_camera(&self.camera);
-
         self.map.draw();
         self.player.draw(4.0);
+
+        set_camera(&self.camera);
         
         for enemy in &mut self.enemies {
             enemy.draw(1.0);
