bb31aca5c7415d6eef83dde718da5a12fc100f8f
Fulvio Esposito <esposito.fulvio@gmail.com>
2024-07-25 16:45:15 +0200

    Set texture filtering to "Nearest"
    In pixel art style games, the default texture filter (Linear) makes
    sprites blurry and can present rendering artificats due to interpolation,
    while nearest-neighbour filtering keeps the visual art as intended.

--- src/game/game.rs
+++ src/game/game.rs
@@ -1,7 +1,7 @@
 use super::*;
+use camera::Viewport;
 use collections::storage;
 use map::{Map, TILE_SIZE};
-use camera::Viewport;
 
 pub struct Game(World);
 
@@ -10,35 +10,40 @@ impl Game {
         let mut world = World::new();
         storage::store(WorldTime(get_time()));
 
+        let player_tex = load_texture("assets/32rogues/rogues.png").await.unwrap();
+        player_tex.set_filter(FilterMode::Nearest);
         let player_id = world.add_entity((
             Position(Vec2::ZERO),
             Velocity(Vec2::ZERO),
             Sprite {
-                tex: load_texture("assets/32rogues/rogues.png").await.unwrap(),
+                tex: player_tex,
                 frame: ivec2(1, 4),
             },
             Moving(false),
             TargetPosition(Vec2::ZERO),
             Health(100.0),
-            Damage(5.0)
+            Damage(5.0),
         ));
 
         let monster_tex = load_texture("assets/32rogues/monsters.png").await.unwrap();
-        let _monster_ids = world.bulk_add_entity((0..199).map(|_| {(
-            Monster,
-            Position(vec2(
-                rand::gen_range(0.0, 64.0 * TILE_SIZE.x),
-                rand::gen_range(0.0, 64.0 * TILE_SIZE.y),
-            )),
-            Velocity(Vec2::ZERO),
-            Sprite {
-                tex: monster_tex.clone(),
-                frame: ivec2(rand::gen_range(0, 1), rand::gen_range(0, 7)),
-            },
-            Moving(false),
-            TargetPosition(Vec2::ZERO),
-            Health(50.0)
-        )}));
+        monster_tex.set_filter(FilterMode::Nearest);
+        let _monster_ids = world.bulk_add_entity((0..199).map(|_| {
+            (
+                Monster,
+                Position(vec2(
+                    rand::gen_range(0.0, 64.0 * TILE_SIZE.x),
+                    rand::gen_range(0.0, 64.0 * TILE_SIZE.y),
+                )),
+                Velocity(Vec2::ZERO),
+                Sprite {
+                    tex: monster_tex.clone(),
+                    frame: ivec2(rand::gen_range(0, 1), rand::gen_range(0, 7)),
+                },
+                Moving(false),
+                TargetPosition(Vec2::ZERO),
+                Health(50.0),
+            )
+        }));
 
         world.add_unique(Map::new("assets/map.json", "assets/tiles.png").await);
         world.add_unique(Viewport::new(screen_width(), screen_height()));
